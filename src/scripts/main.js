95a93af78d729cc4b516095108baf23734a1c282
caio <caiodiasol@proton.me>
2024-10-18 21:05:45 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log("ola mundo");
+AOS.init();
+
+const dataDoEvento = new Date("May 7, 2025 22:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contadorDeHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual; 
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento =  Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+    const minutosAteOEvento =  Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs)/1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contadorDeHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000)
